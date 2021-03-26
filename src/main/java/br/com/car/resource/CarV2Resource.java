3000c13f0db022827e13c861672a1e80f47b74ee
Danilo Catapan <dcatapan@gmail.com>
2021-03-26 14:16:50 -0300

    add model, repository and example sql info

--- src/main/java/br/com/car/resource/CarV2Resource.java
+++ src/main/java/br/com/car/resource/CarV2Resource.java
@@ -1,11 +1,12 @@
 package br.com.car.resource;
 
 import br.com.car.model.Car;
+import br.com.car.repository.CarRepository;
 import io.quarkus.hibernate.orm.panache.PanacheQuery;
 import io.quarkus.panache.common.Page;
 import io.quarkus.panache.common.Parameters;
-import io.quarkus.panache.common.Sort;
 
+import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -14,87 +15,74 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-@Path("/cars")
+@Path("/cars/v2")
 @Produces(MediaType.APPLICATION_JSON)
-public class CarResource {
+public class CarV2Resource {
+
+    @Inject
+    private CarRepository carRepository;
 
     @GET
     public List<Car> listAll() {
-        return Car.listAll();
+        return carRepository.listAll();
     }
 
     @GET
     @Path("/isAvaliableSale")
     public List<Car> listAllCarToSale() {
-        return Car.list("isAvailableSale", true);
+        return carRepository.listAllCarToSale();
     }
 
     @GET
     @Path("/listCarSortNameAndBrand")
     public List<Car> listCarSortNameAndBrand() {
-        return Car.list("order by name, brand");
+        return carRepository.listCarSortNameAndBrand();
     }
 
     @GET
     @Path("/listCarsByYear")
     public List<Car> listCarsByYear(@QueryParam("year") int year) {
-        return Car.find("year(modelYear) = :year", Parameters.with("year", year)).list();
+        return carRepository.listCarsByYear(year);
     }
 
     @GET
     @Path("/count")
     public Long count() {
-        return Car.count();
+        return carRepository.countCar();
     }
 
     @GET
     @Path("/countCarsAvaiableSale")
     public Long countCarsAvaiableSale() {
-        return Car.count("isAvailableSale", true);
+        return carRepository.countCarsAvaiableSale();
     }
 
     @GET
     @Path("/listByPage")
     public List<Car> listCarByPage(@QueryParam("page") int page, @QueryParam("size") int size) {
-        PanacheQuery<Car> listCars = Car.find("isAvailableSale", true);
-        return listCars.page(Page.of(page, size)).list();
+        return carRepository.listCarByPage(page, size);
     }
 
     @POST
-    @Transactional
     public Response create(Car car) {
-        Car.persist(car);
+        Car carEntity = carRepository.save(car);
         return Response.ok(car).status(Response.Status.CREATED).build();
     }
 
     @PUT
     @Path("{id}")
-    @Transactional
     public Response update(@PathParam("id") Long id, Car car) {
 
-        Car carEntity = Car.findById(id);
+        Car carUpdated = carRepository.update(id, car);
 
-        if (carEntity == null) {
-            throw new WebApplicationException("Car with id of " + id + " does not exist.", Response.Status.NOT_FOUND);
-        }
-
-        carEntity.setName(car.getName());
-        carEntity.setBrand(car.getBrand());
-
-        return Response.ok(carEntity).build();
+        return Response.ok(carUpdated).build();
     }
 
     @DELETE
     @Path("{id}")
-    @Transactional
-    public Response delete(@PathParam("id") Long id) {
-        Car carEntity = Car.findById(id);
-
-        if (carEntity == null) {
-            throw new WebApplicationException("Car with id " + id + " does not exist.", Response.Status.NOT_FOUND);
-        }
-
-        carEntity.delete();
+    public Response remove(@PathParam("id") Long id) {
+        carRepository.remove(id);
         return Response.status(Response.Status.NO_CONTENT).build();
     }
+
 }
